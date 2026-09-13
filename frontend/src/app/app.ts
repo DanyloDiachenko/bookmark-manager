2e980fcd59fbd1d44c1732e00996c9acee9503ba
Danil <danildiachenko23@gmail.com>
2026-09-13 17:00:43 +0300

    aside html

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Aside } from './components/aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
