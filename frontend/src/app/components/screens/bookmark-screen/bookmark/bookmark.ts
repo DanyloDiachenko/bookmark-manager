193f81944ee5e85a1976c300ade98dff6f9a284f
Danil <danildiachenko23@gmail.com>
2026-09-20 00:06:12 +0300

    delete

--- frontend/src/app/components/screens/bookmark-screen/bookmark/bookmark.ts
+++ frontend/src/app/components/screens/bookmark-screen/bookmark/bookmark.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { IBookmark } from '../bookmarks.types';
+import { BookmarksService } from '../bookmarks.service';
 
 @Component({
   selector: 'app-bookmark',
@@ -8,5 +9,10 @@ import { IBookmark } from '../bookmarks.types';
   styleUrl: './bookmark.css',
 })
 export class Bookmark {
+  private readonly bookmarkService = inject(BookmarksService);
   readonly bookmark = input.required<IBookmark>();
+
+  public deleteBookmark(id: string) {
+    this.bookmarkService.delete(id).subscribe();
+  }
 }
